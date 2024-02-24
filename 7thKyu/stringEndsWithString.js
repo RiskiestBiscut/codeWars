1a7c67086f8803564e025dea6c905adcd3f44397
riskiestbiscut <sa9smith@uwaterloo.ca>
2024-02-23 20:39:05 -0500

    completd 7th kata

---
+++ 7thKyu/stringEndsWithString.js
@@ -0,0 +1,10 @@
+//params: two strings, check if first ends with second
+//return true or false
+
+function solution(str, ending){
+    return str.endsWith(ending) ? true : false
+    
+  
+  }
+  
+  //example solution("abcde", "cde") -> true
