c6cd7f9740c18d7b5d361dd3aa90f4f83c6e6c19
riskiestbiscut <sa9smith@uwaterloo.ca>
2024-03-25 08:11:09 -0400

    add 8th kyu

---
+++ 8thKyu/doYouPlayBanjo.js
@@ -0,0 +1,8 @@
+function areYouPlayingBanjo(name) {
+    if (name.toLowerCase()[0] === 'r') {
+      return `${name} plays banjo`
+    } else {
+      return `${name} does not play banjo`
+    }
+  
+  }
